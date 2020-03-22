35d4cd701bb9f1795b9eb30e8a81efaf35146ca0
Walmik <walmik.deshpande@gmail.com>
2020-03-21 19:36:22 -0700

    Enable sizzle

--- index.js
+++ index.js
@@ -1,31 +1,82 @@
 /*global post:true*/
 const scribble = require('scribbletune');
 
-module.exports = function(scaleAndPattern) {
-  post(scaleAndPattern);
+const sizzleDict = {
+  none: false,
+  sin: 'sin',
+  cos: 'cos',
+  saw: 'rampUp',
+  'saw-inv': 'rampDown'
+};
+
+module.exports = function(scalePatternAbSubdiv) {
+  post(scalePatternAbSubdiv);
   post('\n');
-  const scaleAndPatternArr = scaleAndPattern.split(',');
-  const scale = scaleAndPatternArr[0] || 'C3 phrygian';
-  const pattern = scaleAndPatternArr[1] || 'x-xR-xRR';
+  const scalePatternAbSubdivArr = scalePatternAbSubdiv.split(',');
+  const scale = scalePatternAbSubdivArr[0] || 'C3 phrygian';
+  const pattern = scalePatternAbSubdivArr[1] || 'x-xRx_RR';
+  const ab = scalePatternAbSubdivArr[2] || 'AAAB'; // AAAB AB A AABB BAAA ABA BAB AABC ABBC ABCC ABC BAC CBA
+  const subdiv = scalePatternAbSubdivArr[3] || 0;
+  const sizzle = sizzleDict[scalePatternAbSubdivArr[4] || 'sin'];
+  const sizzleReps = scalePatternAbSubdivArr[5] || 1;
   post(scale);
   post('\n');
   post(pattern);
   post('\n');
+  post(sizzle);
+  post('\n');
+  post(sizzleReps);
+  post('\n');
+  post('generate');
 
   const mode = scribble.scale(scale);
   const clipA = scribble.clip({
     notes: mode[0],
     pattern,
-    randomNotes: mode.slice(2),
-    sizzle: true
+    randomNotes: mode.slice(1),
+    subdiv,
+    sizzle,
+    sizzleReps
   });
 
   const clipB = scribble.clip({
     notes: mode[0],
     pattern,
-    randomNotes: mode.slice(3)
+    randomNotes: mode.slice(2),
+    subdiv,
+    sizzle,
+    sizzleReps
   });
-  const clip = [...clipA, ...clipA, ...clipA, ...clipB];
+
+  const clipC = scribble.clip({
+    notes: mode[1],
+    pattern,
+    randomNotes: mode.slice(3),
+    subdiv,
+    sizzle,
+    sizzleReps
+  });
+
+  const clip = (() => {
+    // AAAB AB A AABB BAAA ABA BAB AABC ABBC ABCC ABC BAC CBA
+    return ab.split('').reduce((acc, b) => {
+      switch (b) {
+        case 'A':
+          acc = [...acc, ...clipA];
+          break;
+
+        case 'B':
+          acc = [...acc, ...clipB];
+          break;
+
+        case 'C':
+          acc = [...acc, ...clipC];
+          break;
+      }
+
+      return acc;
+    }, []);
+  })();
 
   scribble.max(clip);
 };
